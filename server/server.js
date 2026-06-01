8ab49cee64c78e476af8ed870daa1ac7b2678f15
Kiran Kumari <kirankumari34946491@gmail.com>
2026-06-01 22:12:16 +0530

    initialize server setup

---
+++ server/server.js
@@ -0,0 +1,14 @@
+
+console.log("Starting server...");
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
